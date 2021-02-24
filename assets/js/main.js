9e254d30e8dbf6275921b90cc50cff2ea9400da0
Ngugisenior <ngugijoseph992@gmail.com>
2021-02-24 13:03:29 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,17 @@ window.onload = function(){
     });
 
 
-  /*  // Handle Form Submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    /*  // Handle Form Submission
+        const handleSubmit = (e) => {
+            e.preventDefault();
 
-        let name = document.getElementById('name');
-        let email = document.getElementById('email');
-        let message = document.getElementById('message');
-        let formData = new FormData({name, email, message});
-        // TODO: complete form data processing
-    }
-*/
+            let name = document.getElementById('name');
+            let email = document.getElementById('email');
+            let message = document.getElementById('message');
+            let formData = new FormData({name, email, message});
+            // TODO: complete form data processing
+        }
+    */
 
     //Provide Window Height Per section
     const x = document.getElementsByClassName('h');
@@ -33,30 +33,30 @@ window.onload = function(){
         })
     })
 
-// set box-shadow on window scroll
-window.addEventListener('scroll', function(){
+    // set box-shadow on window scroll
+    window.addEventListener('scroll', function(){
 
-    const styles = `box-shadow:  5px 5px 10px #105a1f,-5px -5px 10px #40ff7b; background-color:#000000;`;
+        const styles = `box-shadow:  5px 5px 10px #105a1f,-5px -5px 10px #40ff7b; background-color:#000000;`;
 
-    if(window.pageYOffset>20){
-        document.getElementsByClassName('nav')[0].style= styles;
+        if(window.pageYOffset>20){
+            document.getElementsByClassName('nav')[0].style= styles;
+        }
+        else{
+            document.getElementsByClassName('nav')[0].style=`box-shadow:  0;`
+        }
+    })
+
+    // Lazy Loading Image
+    const imageLazyLoad = () =>{
+        const x = document.body.getElementsByTagName('img');
+
+        for (let i in x){
+            x[i].setAttribute('loading', 'lazy');
+        }
     }
-    else{
-        document.getElementsByClassName('nav')[0].style=`box-shadow:  0;`
-    }
-})
-
-// Lazy Loading Image
-const imageLazyLoad = () =>{
-    const x = document.body.getElementsByTagName('img');
-
-    for (let i in x){
-        x[i].setAttribute('loading', 'lazy');
-    }
-}
 
 
-    async function fetchProducts(){
+    (async function fetchProducts(){
         const products = document.querySelector('.products_listing');
 
         const response = await fetch('/assets/json/main.json');
@@ -73,13 +73,15 @@ const imageLazyLoad = () =>{
             div.appendChild(img);
             products.appendChild(div);
         })
-    }
+    })();
+
 
 
     
 
+
     
-fetchProducts();
+
 }
 
 // Handle Button Clicks
